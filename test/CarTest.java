033eb461931b0e3be15002e6eed03706d4d55561
jtalford4 <jtalford4@student.cvtc.edu>
2016-02-01 18:45:00 -0600

    Created canDecelerate() unit test

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,6 +32,13 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
+	@Test
+	public void canDecelerate(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(100);
+		car1.decelerate(50);
+		assertThat(car1.getSpeed(), is(50));
+	}
 	
 	// Create and pass test for no deceleration below 0.
 
